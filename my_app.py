c00a9414a1ecac1a3b2029869a79cea5d7705ecc
SaniyaMamti2020 <mamtisaniya@gmail.com>
2025-09-06 10:58:29 +0530

    LHC

---
+++ my_app.py
@@ -0,0 +1,8 @@
+import humans
+
+# print("\nMother : ",humans.mothers)
+# print("\nFather : ",humans.fathers)
+# print("\nChildren : ",humans.children)
+
+jacob = humans.Jacob()
+print("\n Children :",jacob.children)
